27666c370219cb2618ada95e0eca6cc77ede1b06
ethan <ethan.fifle@gmail.com>
2024-04-23 00:07:47 -0400

    API refactor

--- src/Services/TeckPackBE.tsx
+++ src/Services/TeckPackBE.tsx
@@ -1,33 +1,28 @@
 class ApiService {
-    async uploadFile(formData: FormData): Promise<string> {
-        try {
-            const response = await fetch('http://localhost:5000/upload', {
-                method: 'POST',
-                body: formData,
-            });
+  async uploadFile(formData: FormData): Promise<string> {
+    try {
+      const response = await fetch('http://localhost:5000/upload', {
+        method: 'POST',
+        body: formData,
+      });
 
-            if (response.ok) {
-                const responseBody = await response.text();
-                return `Success: ${responseBody}`;
-            } else {
-                return `Server error: ${response.status} ${response.statusText}`;
-            }
-        } catch (error) {
-            console.error('Error submitting form:', error);
-            throw new Error('Failed to upload the file.');
-        }
+      if (!response.ok) {
+        throw new Error(`Server error: ${response.status} ${response.statusText}`);
+      }
+
+      const jsonResponse = await response.json();
+
+      console.log(jsonResponse);
+      if (jsonResponse.status === "success") {
+        return jsonResponse.data;
+      } else {
+        throw new Error(`Server returned an error status: ${jsonResponse.status}`);
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      throw new Error('Failed to upload the file.');
     }
-
-    // Example placeholders for other API methods
-    /*async fetchData(url: string): Promise<any> {
-        // Implement GET request
-    }
-
-    async updateData(url: string, data: any): Promise<any> {
-        // Implement PUT request
-    }
-
-     */
+  }
 }
 
 export const apiService = new ApiService();
